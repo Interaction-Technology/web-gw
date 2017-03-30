ea0eabc9506b9ca46623abe9a82ef14aadac69ae
felled <931430246@qq.com>
2017-03-30 18:03:01 +0800

    modified index.html

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,16 +1,32 @@
 $(function(){
 	$(".ours-addr-node li").click(function(){
-	$(".ours-addr-node li").removeClass("atcive")
+	$(".ours-addr-node li").removeClass("atcive");
 	$(this).addClass("atcive")
 	})
 	
+	//合作弹窗
 	$(".collaboration").click(function(){
-		$(".cooperation").css("display","block")
+		$(".cooperation").css("display","block");
 		return false;
 	})
 	
 	$(".close").click(function(){
-		$(".cooperation").css("display","none")
+		$(".cooperation").css("display","none");
 		return false;
 	})
+	//侧边栏弹窗
+	 $(".share li").click(function(){
+	 	$(".share li").children('dl').css("display","none");
+	 	$(this).children('dl').css("display","block");
+	 })
+	  $(".share li .w-close").click(function(){
+	  	$(this).parent().parent().css("display","none");
+	  	return false;
+	  })
+	 
+	 
+	  
 })
+
+ 
+ 
