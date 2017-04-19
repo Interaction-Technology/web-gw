933c698355a0f7dd6ff2f6e354ec041bea3223b1
bill liu <396700613@qq.com>
2017-04-19 09:37:16 +0800

    修改弹窗

--- dist/js/base.js
+++ dist/js/base.js
@@ -1,47 +1,50 @@
 $(function() {
     // 顶部导航
     var url = window.location.href;
-
-    $('nav li').removeClass('nav-ate');
-
-    if(url.indexOf('index.html') >= 0 ){
-        $('nav li').eq(0).addClass('nav-ate');
+    // 载入页面后清楚高亮样式
+    $('#nav li').removeClass('nav-ate');
+    // 判断当前页面链接是否包含所需关键词
+    if (url.indexOf('index.html') >= 0) { //首页
+        // 切换到当前页面所对应标签并高亮
+        $('#nav li').eq(0).addClass('nav-ate');
     };
-    if(url.indexOf('service.html') >= 0 ){
-        $('nav li').eq(1).addClass('nav-ate');
+    if (url.indexOf('service.html') >= 0) { //服务
+        $('#nav li').eq(1).addClass('nav-ate');
     };
-    if(url.indexOf('achievements.html') >= 0 ){
-        $('nav li').eq(2).addClass('nav-ate');
+    if (url.indexOf('achievements.html') >= 0) { //成果
+        $('#nav li').eq(2).addClass('nav-ate');
     };
-    if(url.indexOf('ours.html') >= 0 ){
-        $('nav li').eq(3).addClass('nav-ate');
+    if (url.indexOf('ours.html') >= 0) { //我们
+        $('#nav li').eq(3).addClass('nav-ate');
     };
-    if(url.indexOf('class.html') >= 0 ){
-        $('nav li').eq(4).addClass('nav-ate');
+    if (url.indexOf('class.html') >= 0) { //课程
+        $('#nav li').eq(4).addClass('nav-ate');
     };
 
     //合作弹窗
     $('.collaboration').click(function() {
-        $('.cooperation').removeClass('cooperation-hide animatedHide')
+        // 清除隐藏动画
+        $('.cooperation').removeClass('cooperation-hide animatedHide');
+        // 激活后添加显示动画
         $('.cooperation').show().addClass('cooperation-show animatedShow');
-        return false;
-    })
-
-    $('.close').click(function() {
-        $('.cooperation').removeClass('cooperation-show animatedShow');
-        $('.cooperation').addClass('cooperation-hide animatedHide');
-        return false;
+        // return false;
     });
 
-
+    $('.close').click(function() {
+        // 关闭后移除显示动画
+        $('.cooperation').removeClass('cooperation-show animatedShow');
+        // 添加隐藏动画
+        $('.cooperation').addClass('cooperation-hide animatedHide');
+        // return false;
+    });
 
 
 
     //map 呼吸灯   
     $('.ours-addr-node li').click(function() {
         $('.ours-addr-node li').removeClass('atcive');
-        $(this).addClass('atcive')
-    })
+        $(this).addClass('atcive');
+    });
 
     $('#cale li').click(function() {
         var cls = $(this).attr('type'); //获取attr
@@ -58,18 +61,13 @@ $(function() {
 
         });
         e.css({
-                'margin-top': '0px',
-                'opacity': '1',
-            })
-            //地址右侧
-        // var text = $(this).children().text();
-        // $(this).siblings().children().show();
-        // $(this).children().hide();
-
+            'margin-top': '0px',
+            'opacity': '1',
+        });
+        // 地址右侧
         var text = $(this).children().text();
-        $(this).siblings().children().css("display","block");
-        $(this).children().css("display","none");
-
+        $(this).siblings().children().show();
+        $(this).children().hide();
     });
 
 
@@ -82,4 +80,52 @@ $(function() {
         $(this).parent().parent().hide();
         return false;
     });
-})
+
+
+    // 我要合作表单
+    // $('#submitForm').validate({
+    //     /**/
+    //     /* 设置验证规则 */
+    //     rules: {
+    //         username: {
+    //             required: true,
+    //             stringCheck: true,
+    //             byteRangeLength: [3, 15]
+    //         },
+    //         phone: {
+    //             required: true,
+    //             isPhone: true
+    //         },
+    //     },
+
+    //     /**/
+    //     /* 设置错误信息 */
+    //     messages: {
+    //         username: {
+    //             required: "请填写用户名",
+    //             stringCheck: "用户名只能包括中文字、英文字母、数字和下划线",
+    //             byteRangeLength: "用户名必须在3-15个字符之间(一个中文字算2个字符)"
+    //         },
+    //         email: {
+    //             required: "请输入一个Email地址",
+    //             email: "请输入一个有效的Email地址"
+    //         },
+    //         phone: {
+    //             required: "请输入您的联系电话",
+    //             isPhone: "请输入一个有效的联系电话"
+    //         },
+    //     },
+
+    //     /**/
+    //     /* 设置验证触发事件 */
+    //     focusInvalid: false,
+    //     onkeyup: false,
+
+    //     /**/
+    //     /* 设置错误信息提示DOM */
+    //     errorPlacement: function(error, element) {
+    //         error.appendTo(element.parent());
+    //     },
+
+    // });
+});
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,12 +1,12 @@
 //重置banner高度
 var heig = $(window).height();
-var banImg = document.getElementById("banner");
-banImg.style.height = heig + "px";
+var banImg = document.getElementById('banner');
+banImg.style.height = heig + 'px';
 
 // $(function() {
 //     //nav点击事件
-//     $("nav ul li").click(function() {
-//         $("nav ul li").removeClass('nav-ate');
+//     $('nav ul li').click(function() {
+//         $('nav ul li').removeClass('nav-ate');
 //         $(this).addClass('nav-ate');
 //     })
 // });
@@ -16,20 +16,20 @@ $(window).scroll(function() {
     var top = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop; //滚轮滚动的距离
     var heig = $(window).height();
     if (top < 1) {
-        $(".banner").css("margin-top", "0px");
-        $(".share").hide();
+        $('.banner').css('margin-top', '0px');
+        $('.share').hide();
     };
     if (top > 1) {
-        $(".banner").css("margin-top", -heig + "px")
-        $(".share").show();
+        $('.banner').css('margin-top', -heig + 'px')
+        $('.share').show();
         //局部刷新
-        $(".map").load(location.href + " .map");
+        $('.map').load(location.href + ' .map');
     };
     if (top = heig) {
-        $(".cnt-service ul li").css({
-            // "margin-top": "0px",
-            "top": "0",
-            "opacity": "1",
+        $('.cnt-service ul li').css({
+            // 'margin-top': '0px',
+            'top': '0',
+            'opacity': '1',
         })
     };
 });
@@ -78,26 +78,26 @@ $(document).mousemove(function(e) {
 });
 
 function moveDiv(X, Y, S, RX, RY) {
-    var ban = $("#banner-gif");
-    var banText = $("#banner-gif2");
+    var ban = $('#banner-gif');
+    var banText = $('#banner-gif2');
     var banCss = 'translate(' + X / 4 + 'px,' + Y / 4 + 'px) rotateX(' + RX + 'deg)';
     var banTextCss = 'translate(' + X / 2 + 'px,' + Y / 2 + 'px) rotateX(' + SRX + 'deg)';
-    ban.css("transform", banCss);
-    banText.css("transform", banTextCss);
+    ban.css('transform', banCss);
+    banText.css('transform', banTextCss);
     return false;
 };
 
 // 成果轮播图
 // 轮播参考：（http://www.superslide2.com/demo.html）- 2.焦点图／幻灯片
-jQuery("#projects").slide({
+jQuery('#projects').slide({
     // 获取节点
-    mainCell: ".bd ul",
+    mainCell: '.bd ul',
     // 向左循环滚动
-    effect: "leftLoop",
+    effect: 'leftLoop',
     // 自动播放
     autoPlay: true,
     // 滚动效果（参考：http://www.superslide2.com/otherDemo/T2.1/easing.html）
-    easing: "easeInQuad",
+    easing: 'easeInQuad',
     // 滚动经历时间
     delayTime: 500,
     // 间隔时间
